6d98f025c46df821207e4c4cfd310b598d65d042
Ilia Shevrin <iliashevrin@gmail.com>
2026-05-03 10:25:11 +0300

    print

--- simulate_user.py
+++ simulate_user.py
@@ -86,7 +86,8 @@ if __name__ == "__main__":
     else:
         raise("Incorrect example generation function")
 
-    seen = 0
+    seen_in_success = 0
+    seen_in_failure = 0
     success = 0
     total = 0
 
@@ -107,19 +108,24 @@ if __name__ == "__main__":
 
             traces_seen, props = simulate_user(candidate, ground_truth, func)
 
-            seen += traces_seen
             # Incorrect candidate was successfully rejected, look at the props of the discriminating trace
             if props is not None:
                 success += 1
+                seen_in_success += traces_seen
 
                 if props not in props_map:
                     props_map[props] = 1
                 else:
                     props_map[props] += 1
 
+            else:
+                seen_in_failure += traces_seen
 
-    print(seen / total)
-    print(success / total)
+    print(f'Total Formulas: {total}')
+    print(f'Total Detected: {success}')
+    print(f'Detection Ratio: {success / total}')
+    print(f'Average Inspected Traces Until Detection: {seen_in_success / success}')
+    print(f'Average Inspected Traces When No Detection: {seen_in_failure / (total - success)}')
 
     for prop in props_map:
         print(f'{str(prop)}:{props_map[prop]}')
